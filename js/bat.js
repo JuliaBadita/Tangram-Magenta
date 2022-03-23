413949720a13f7c8846df995fb4310277b03a7c5
julia <juliabadita@live.fr>
2022-03-23 15:09:30 +0100

    adding the animation on the bat

--- js/bat.js
+++ js/bat.js
@@ -1,1 +1,6 @@
+console.log('loaded');
+const bat = document.querySelector("#bat");
+bat.addEventListener("click", function (e) {
+    document.getElementById("figure").classList.toggle("bat");
+   });
 
