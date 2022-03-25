0c91458ed8670a686b8833283efdc736ed7f7b43
ReginAstris <paulinemerlin410@gmail.com>
2022-03-25 13:30:10 +0100

    modify responsive + fix error menu

--- js/fox.js
+++
@@ -1,5 +0,0 @@
-console.log('loaded');
-const fox = document.querySelector(".button-fox");
-fox.addEventListener("click", function (e) {
-    document.getElementById("figure").classList.toggle("fox");
-});
